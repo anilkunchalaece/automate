46c0b1314a62235b17dca664c5710c2f6a3f2b9d
anilkunchalaece <anlkunchalaece@gmail.com>
2017-02-26 19:57:21 +0530

    added gTTS and pygame for voice

--- test.py
+++ test.py
@@ -1,13 +1,26 @@
 
-import pyttsx
+#import pyttsx
+from gtts import gTTS
+import pygame
 import getMessages
 
-engine = pyttsx.init();
-rate = engine.getProperty('rate')
-engine.setProperty('rate', rate-65)
 
+
+#engine = pyttsx.init();
+#rate = engine.getProperty('rate')
+#engine.setProperty('rate',70)
 val = getMessages.main()
 print(val)
-output = " Hi There Anil... Got Message From" + val[0]['sender']+"Subject is" + val[0]['subject']
-engine.say(output)
-engine.runAndWait()
+output = 'Hi Anil you got Email from '+val[0]['sender']+" Subject is " + val[0]['subject']
+print(output)
+tts = gTTS(text=output, lang='en')
+
+tts.save("hello.mp3")
+#import vlc
+#p = vlc.MediaPlayer("hello.mp3")
+#p.play()
+pygame.mixer.init()
+pygame.mixer.music.load("hello.mp3")
+pygame.mixer.music.play()
+while pygame.mixer.music.get_busy() == True:
+    continue
